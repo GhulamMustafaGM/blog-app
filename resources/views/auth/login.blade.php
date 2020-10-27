d0c3ff429a1e2d4d0c4479af04be23a3ed6f192a
Ghulam Mustafa <mustafaji@gmail.com>
2020-10-27 08:24:47 +0100

    GM commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,92 +1,10 @@
 
+@extends('layouts.auth')
 
-<!-- <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>   
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div> -->
-
-@extends('layouts.app')
+@section('title') Login @endsection
 
 @section('content')
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hemdel - Admin </title>
-    <link rel="stylesheet" href="{{ asset('admin/assets/vendor/simple-line-icons/css/simple-line-icons.css') }}">
-    <link rel="stylesheet" href="{{ asset('admin/assets/vendor/font-awesome/css/fontawesome-all.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('admin/assets/css/styles.css') }}">
-</head>
-<body>
-<div class="page-wrapper flex-row align-items-center">
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-5">
@@ -100,7 +18,7 @@
                         <div class="card-body py-5">
                         <div class="form-group">
                             <label class="form-control-label">Email</label>
-                            <input type="email" name="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}">
+                        <input type="email" name="email" value="{{ old('email') }}" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}">
                         @if($errors->has('email'))
                             <span class="invalid-feedback">
                                 <strong> {{ $errors->first('email')}} </strong>
@@ -135,20 +53,11 @@
                             </div>
                         </div>
                     </div>
+                    </form>
+
                 </div>
             </div>
         </div>
     </div>
-</div>
-<script src="./vendor/jquery/jquery.min.js"></script>
-<script src="./vendor/popper.js/popper.min.js"></script>
-<script src="./vendor/bootstrap/js/bootstrap.min.js"></script>
-<script src="./vendor/chart.js/chart.min.js"></script>
-<script src="./js/carbon.js"></script>
-<script src="./js/demo.js"></script>
-</body>
-</html>
-
-
 
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,9 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
+@section('register') Register @endsection
 @section('content')
+
+<!--
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -74,4 +77,62 @@
         </div>
     </div>
 </div>
+-->
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-5">
+            <div class="card p-4">
+                <div class="card-header text-center text-uppercase h4 font-weight-light">
+                    Register
+                </div>
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
+
+                <div class="card-body py-5">
+                    <div class="form-group">
+                        <label class="form-control-label">Name</label>
+                    <input type="text" name="name" value="{{ old('name') }}" class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}">
+                        @if($errors->has('name'))
+                        <span class="invalid-feedback">
+                            <strong> {{ $errors->first('name')}} </strong>
+                        </span>
+                    @endif
+                    </div>
+
+                    <div class="form-group">
+                        <label class="form-control-label">Email</label>
+                        <input type="email" name="email" value="{{ old('email') }}" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }} ">
+                        @if($errors->has('email'))
+                            <span class="invalid-feedback">
+                                <strong> {{ $errors->first('email')}} </strong>
+                            </span>
+                        @endif
+                    </div>
+
+                    <div class="form-group">
+                        <label class="form-control-label">Password</label>
+                        <input type="password" class="form-control  {{ $errors->has('password') ? 'is-invalid' : '' }}" name="password">
+                        @if($errors->has('password'))
+                            <span class="invalid-feedback">
+                                <strong> {{ $errors->first('password')}} </strong>
+                            </span>
+                        @endif
+                    </div>
+
+                    <div class="form-group">
+                        <label class="form-control-label">Confirm Password</label>
+                        <input type="password" name="password_confirmation" class="form-control">
+                    </div>
+                </div>
+
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-success btn-block">Create Account</button>
+                </div>
+            </form>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
