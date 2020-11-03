a49940f63067b7d29fd2cae68ab05e0a5d88e91a
Ghulam Mustafa <mustafaji@gmail.com>
2020-11-03 22:24:21 +0100

    GM commit

--- resources/views/admin/posts.blade.php
+++ resources/views/admin/posts.blade.php
@@ -44,4 +44,28 @@
         </div>
     </div>
 </div>
+
+@foreach ($posts as $post)
+    <!-- Modal -->
+    <div class="modal fade" id="deletePostModal-{{ $post->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+            <h5 class="modal-title" id="myModalLable">You are about to delete {{ $post->title }}.</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            </div>
+            <div class="modal-body">
+                Are you sure?
+            </div>
+            <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">No, keept it</button>
+            <button type="button" class="btn btn-primary">yes, delete it</button>
+            </div>
+        </div>
+        </div>
+    </div>
+@endforeach
+
 @endsection
