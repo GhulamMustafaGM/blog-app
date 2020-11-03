d6d147855ffef4ae552cb5afe9ac43168cef39ab
Ghulam Mustafa <mustafaji@gmail.com>
2020-11-03 23:17:20 +0100

    GM commit

--- resources/views/admin/posts.blade.php
+++ resources/views/admin/posts.blade.php
@@ -45,25 +45,24 @@
     </div>
 </div>
 
-@foreach ($posts as $post)
+@foreach($posts as $post)
     <!-- Modal -->
-    <div class="modal fade" id="deletePostModal-{{ $post->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
+    <div class="modal fade" id="deletePostModal-{{ $post->id }}" tabindex="-1" role ="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-            <h5 class="modal-title" id="myModalLable">You are about to delete {{ $post->title }}.</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-            </div>
+            <h5 class="modal-title" id="myModalLable">You are about to delete {{ $post->title }}.</h5>    
+        </div>
             <div class="modal-body">
                 Are you sure?
             </div>
             <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">No, keept it</button>
+            <button type="button" class="btn btn-default" data-dismiss="modal">No, keept it</button>
+            <form method="POST" id="deletePost-{{ $post->id }}" action="{{ route('adminDeletePost', $post->id) }}">@csrf
             <button type="button" class="btn btn-primary">yes, delete it</button>
+            </form>
             </div>
-        </div>
+            </div>
         </div>
     </div>
 @endforeach
