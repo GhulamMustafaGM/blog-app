bc8ac653e995852d5ac3f47dbf066a8372acf1ef
Ghulam Mustafa <mustafaji@gmail.com>
2020-11-04 23:45:53 +0100

    GM commit

--- resources/views/admin/posts.blade.php
+++ resources/views/admin/posts.blade.php
@@ -33,7 +33,7 @@
                                 <td>
                                     <a href="{{ route('adminPostEdit', $post->id) }}" class="btn-warning">Edit</a>
                                     <form method ="POST" id="deletePost-{{ $post->id }}" action="{{ route('adminDeletePost', $post->id) }}">@csrf</form>
-                                    <a href="#" onclick="document.getElementById('deletePost-{{ $post->id }}').submit()" class="btn-danger">Remove</a>
+                                <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deletePostModal-{{$post->id}}"></button>
                                 </td>
                             </tr>
                     @endforeach
@@ -44,6 +44,33 @@
         </div>
     </div>
 </div>
+
+
+
+
+<div class="modal fade" id="deletePostModal-{{ $post->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">You are about to delete {{ $post->title }}.</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            </div>
+            <div class="modal-body">
+            Are you sure? 
+            </div>
+            <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">No, keep it</button>
+            <form method="POST" id="deletePost-{{ $post->id }}" action="{{ route('adminDeletePost', $post->id) }}">@csrf
+            <button type="button" class="btn btn-primary">Yes, delete it</button>
+            </form>
+            </div>
+        </div>
+        </div>
+    </div>
+
+
 
 @foreach($posts as $post)
     <!-- Modal -->
